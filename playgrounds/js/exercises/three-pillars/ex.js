c1b902b10362b1449c795a7009e2541b99f2c001
lethanhan97 <lethanhanzzz@gmail.com>
2020-10-30 22:31:07 +0800

    finished 3 pillars exercise

--- playgrounds/js/exercises/three-pillars/ex.js
+++ playgrounds/js/exercises/three-pillars/ex.js
@@ -1,43 +1,49 @@
 class Bookshelf {
-	constructor() {
-		this.favoriteBooks = [];
-	}
+  constructor() {
+    this.favoriteBooks = [];
+  }
 
-	// TODO: define methods `addFavoriteBook(..)`
-	// and `printFavoriteBooks()`
+  // TODO: define methods `addFavoriteBook(..)`
+  // and `printFavoriteBooks()`
+  addFavoriteBook(bookName) {
+    if (!bookName.includes("Great")) {
+      this.favoriteBooks.push(bookName);
+    }
+  }
+
+  printFavoriteBooks() {
+    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+    for (let bookName of this.favoriteBooks) {
+      console.log(String(bookName));
+    }
+  }
 }
 
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
-	}
-}
-
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
-
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
+function loadBooks(bookShelf) {
+  fakeAjax(BOOK_API, function addBooks(books) {
+    books.forEach(function processBook(book) {
+      bookShelf.addFavoriteBook(String(book));
+    });
+    bookShelf.printFavoriteBooks();
+  });
 }
 
 var BOOK_API = "https://some.url/api";
 
-
 // ***********************
 
 // NOTE: don't modify this function at all
-function fakeAjax(url,cb) {
-	setTimeout(function fakeLoadingDelay(){
-		cb([
-			"A Song of Ice and Fire",
-			"The Great Gatsby",
-			"Crime & Punishment",
-			"Great Expectations",
-			"You Don't Know JS"
-		]);
-	},500);
+function fakeAjax(url, cb) {
+  setTimeout(function fakeLoadingDelay() {
+    cb([
+      "A Song of Ice and Fire",
+      "The Great Gatsby",
+      "Crime & Punishment",
+      "Great Expectations",
+      "You Don't Know JS",
+    ]);
+  }, 500);
 }
+
+const bookShelf = new Bookshelf();
+loadBooks(bookShelf);
